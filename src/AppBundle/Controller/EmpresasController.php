1f36863502722aa0121dc86420db405ecff9da40
Mathi <mathiaseip@gmail.com>
2017-09-23 18:27:09 -0300

    Prueba de integracion del template

--- src/AppBundle/Controller/EmpresasController.php
+++ src/AppBundle/Controller/EmpresasController.php
@@ -13,9 +13,44 @@ class EmpresasController extends Controller
 {
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        // replace this example code with whatever you need ----  render('default/index.html.twig'
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    public function listEmpresas(Request $request){
+     /*   // Cargo los servicios que voy a utilizar.
+        $helpers = $this->get('app.helpers');
+        $serializer = SerializerBuilder::create()->build();
+
+        // Recupero el hash recibido
+        $json = $request->get('authorization', null);
+
+        // Pregunto al servicio si el hash es auténtico.
+        $authCheck = $helpers->authCheck($hash);
+
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery('select u from TblRubros u');
+        $rubros = $query->getResult();
+        $jsonResponse = $serializer->serialize($rubros, 'json');
+        return new Response($jsonResponse);
+    */
+        $helpers = $this->get('app.helpers');
+        $serializer = SerializerBuilder::create()->build();
+        
+        $em = $this->getDoctrine()->getManager();
+        
+        $empresas = $em->getRepository("BackendBundle:TblEmpresas")->findAll();
+        
+        // $rubros = $query->getResult();
+        /*$data = array(
+            'status' => 'ERROR',
+            'msg' => 'El usuario no pudo ser creado.'
+        );
+        */
+        $jsonResponse = $serializer->serialize($empresas, 'json');
+        return new Response($jsonResponse);
+        
     }
 }
