2f8c97bf9c452256d915c29622e62b622c2a98ba
nicolopezcesan <nicolopezcesan@gmail.com>
2017-09-25 12:50:43 -0300

    creacion de empresas

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -32,65 +32,65 @@ class UserController extends Controller
     return new Response($jsonResponse);
 }
 
-public function newAction(Request $request) {
-        // Cargo los servicios que voy a utilizar.
-    $helpers = $this->get('app.helpers');
-    $serializer = SerializerBuilder::create()->build();
+    public function newAction(Request $request) {
+            // Cargo los servicios que voy a utilizar.
+        $helpers = $this->get('app.helpers');
+        $serializer = SerializerBuilder::create()->build();
 
-    $json = $request->get('json', null);
-    $params = json_decode($json);
+        $json = $request->get('json', null);
+        $params = json_decode($json);
 
-        // Por default $data devuelve un error generico.      
-    $data = array(
-      'status' => 'ERROR',
-      'msg' => 'El usuario no pudo ser creado.'
-      );
+            // Por default $data devuelve un error generico.      
+        $data = array(
+          'status' => 'ERROR',
+          'msg' => 'El usuario no pudo ser creado.'
+          );
 
 
-    	// Si $json no es null, procedo a crear el usuario.
-    if ($json != null) {
-       $username = (isset($params->username)) ? $params->username : null;
-       $password = (isset($params->password)) ? $params->password : null;
-       $nombre = (isset($params->nombre)) ? $params->nombre : null;
-       $apellido = (isset($params->apellido)) ? $params->apellido : null;
-       $email = (isset($params->email)) ? $params->email : null;
-       $role = (isset($params->role)) ? $params->role : 1;
-       $activo = 1;
-       $createdAt = new \Datetime('now');
+        	// Si $json no es null, procedo a crear el usuario.
+        if ($json != null) {
+         $username = (isset($params->username)) ? $params->username : null;
+         $password = (isset($params->password)) ? $params->password : null;
+         $nombre = (isset($params->nombre)) ? $params->nombre : null;
+         $apellido = (isset($params->apellido)) ? $params->apellido : null;
+         $email = (isset($params->email)) ? $params->email : null;
+         $role = (isset($params->role)) ? $params->role : 1;
+         $activo = 1;
+         $createdAt = new \Datetime('now');
 
-        	// Valido que el Email sea un realmente un email mediante Asserts.
-       $emailContraint = new Assert\Email();
-       $emailContraint->message = "Este email no es válido";
-       $validate_email = $this->get('validator')->validate($email, $emailContraint);
+            	// Valido que el Email sea un realmente un email mediante Asserts.
+         $emailContraint = new Assert\Email();
+         $emailContraint->message = "Este email no es válido";
+         $validate_email = $this->get('validator')->validate($email, $emailContraint);
 
-            // Valido que ninguno de los datos del usuario sea null.
-       if ($email != null && count($validate_email) == 0 && $password != null 
-           && $nombre != null && $apellido != null	&& $role != null) {
+                // Valido que ninguno de los datos del usuario sea null.
+         if ($email != null && count($validate_email) == 0 && $password != null 
+             && $nombre != null && $apellido != null	&& $role != null) {
 
-            	// Instanciamos un objeto User y seteamos sus datos.
-           $user = new TblUser();
+                	// Instanciamos un objeto User y seteamos sus datos.
+             $user = new TblUser();
 
-       $user->setUsername($username);
-       $user->setNombre($nombre);
-       $user->setApellido($apellido);
-       $user->setEmail($email);
-       $user->setRole($role);
-       $user->setActivo($activo);
-       $user->setCreateDate($createdAt);
+         $user->setUsername($username);
+         $user->setNombre($nombre);
+         $user->setApellido($apellido);
+         $user->setEmail($email);
+         $user->setRole($role);
+         $user->setActivo($activo);
+         $user->setCreateDate($createdAt);
 
-            	// Cifrar contraseña
-       $psw = hash('sha256', $password);
-       $user->setPassword($psw);
+                	// Cifrar contraseña
+         $psw = hash('sha256', $password);
+         $user->setPassword($psw);
 
-            	// Busco en la DB si existe un usuario con el email ingresado.
-       $em = $this->getDoctrine()->getManager();
-       $isset_user = $em->getRepository("BackendBundle:TblUser")->findBy(
+                	// Busco en la DB si existe un usuario con el email ingresado.
+         $em = $this->getDoctrine()->getManager();
+         $isset_user = $em->getRepository("BackendBundle:TblUser")->findBy(
           array(
-             'email' => $email
-             ));
+           'email' => $email
+           ));
 
-            	// Si el usuario no existe, se inserta en la DB.
-       if (count($isset_user) == 0) {
+                	// Si el usuario no existe, se inserta en la DB.
+         if (count($isset_user) == 0) {
           $em->persist($user);
           $em->flush();	
 
@@ -99,17 +99,17 @@ public function newAction(Request $request) {
             'msg' => 'El usuario ha sido creado con exito!'
             );
       } else {
-            		// Si el usuario existe, retornamos mensaje de usuario duplicado.
+                		// Si el usuario existe, retornamos mensaje de usuario duplicado.
           $data = array(
             'status' => 'ERROR',
             'msg' => 'El usuario ya se encuentra registrado!'
             );
       }
-  }
-}
+    }
+    }
 
-$jsonResponse = $serializer->serialize($data, 'json');
-return new Response($jsonResponse);
+    $jsonResponse = $serializer->serialize($data, 'json');
+    return new Response($jsonResponse);
 }
 
 
@@ -133,23 +133,23 @@ public function editAction(Request $request) {
       // Busco en la DB si existe un usuario con el email ingresado.
       $isset_email = $em->getRepository("BackendBundle:TblUser")->findBy(
           array(
-           'email' => $identity->email,
-           'id' => $identity->sub
-           ));
+             'email' => $identity->email,
+             'id' => $identity->sub
+             ));
 
       // Busco en la DB si existe un usuario con el username ingresado.
       $isset_username = $em->getRepository("BackendBundle:TblUser")->findBy(
           array(
-           'username' => $identity->username
-           ));
+             'username' => $identity->username
+             ));
 
 
       // Recibimos los datos nuevos del usuario del Request.
-    $json = $request->get('json', null);
-    $params = json_decode($json);
+      $json = $request->get('json', null);
+      $params = json_decode($json);
 
       // Por default $data devuelve un error generico.      
-    $data = array(
+      $data = array(
         'status' => 'ERROR',
         'msg' => 'El usuario no pudo ser editado.'
         );
@@ -158,7 +158,7 @@ public function editAction(Request $request) {
       // FALTA HACER LAS EXCEPCIONES PARA CUANDO QUIERO MODIFICAR UN USUARIO Y ASIGNARLE EL EMAIL O USERNAME DE OTRO USUARIO YA EXISTENTE.
 
       // Si $json no es null, procedo a crear el usuario.
-    if ($json != null) {
+      if ($json != null) {
         $username = (isset($params->username)) ? $params->username : null;
         $nombre = (isset($params->nombre)) ? $params->nombre : null;
         $apellido = (isset($params->apellido)) ? $params->apellido : null;
@@ -192,39 +192,4 @@ $jsonResponse = $serializer->serialize($data, 'json');
 return new Response($jsonResponse);
 }
 
-    // Controller de Santi
-public function listAction(Request $request){
-     /*   // Cargo los servicios que voy a utilizar.
-        $helpers = $this->get('app.helpers');
-        $serializer = SerializerBuilder::create()->build();
-
-        // Recupero el hash recibido
-        $json = $request->get('authorization', null);
-
-        // Pregunto al servicio si el hash es auténtico.
-        $authCheck = $helpers->authCheck($hash);
-
-        $em = $this->getDoctrine()->getManager();
-        $query = $em->createQuery('select u from TblRubros u');
-        $rubros = $query->getResult();
-        $jsonResponse = $serializer->serialize($rubros, 'json');
-        return new Response($jsonResponse);
-    */
-        $helpers = $this->get('app.helpers');
-        $serializer = SerializerBuilder::create()->build();
-        
-        $em = $this->getDoctrine()->getManager();
-        
-        $empresas = $em->getRepository("BackendBundle:TblEmpresas")->findAll();
-        
-        // $rubros = $query->getResult();
-        /*$data = array(
-            'status' => 'ERROR',
-            'msg' => 'El usuario no pudo ser creado.'
-        );
-        */
-        $jsonResponse = $serializer->serialize($empresas, 'json');
-        return new Response($jsonResponse);
-        
-    }
 }
